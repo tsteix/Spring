65e8ca0654f81a315a027f93b52514891e5960e3
Tiago <tiagosantos.t11@gmail.com>
2022-01-21 18:37:23 -0300

    Primeira Atividades utilizando Spring

---
+++ PrimeiraAplicacao/PrimeiraAplicacao/src/main/java/com/generation/PrimeiraAplicacao/Controller/Controller.java
@@ -0,0 +1,20 @@
+package com.generation.PrimeiraAplicacao.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController 
+public class Controller {
+	
+	 //Criação de end-point 
+	@GetMapping ("/mentalidades") // "mentalidades" pagina gerada, ultima parte do link
+	public String mentalidades() {
+		return "Mentalidade de Persistencia pois tive que tentar diversas vezes realizar as atividades envolvendo spring";
+	}
+	
+	@GetMapping ("/objetivos_de_aprendizagem")
+	public String objetivos_de_aprendizagem() {
+		return "Esta semana quero me dedicar em aprender a utilizar as ferramentas propostas nas aulas, como o Spring, e a ler e entender novas documentações. ";
+	}
+	
+}
